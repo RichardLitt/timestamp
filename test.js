dfebb8cdc1750bc1ab59faf850f5fad4a9f974e9
Richard Littauer <richard.littauer@gmail.com>
2015-12-28 18:10:32 -0500

    Fixed test

--- test.js
+++ test.js
@@ -3,6 +3,9 @@ var assert = require('assert');
 var moment = require('moment');
 var timestamp = require('./');
 
+it('should match the format provided', function () {
+	assert.strictEqual(timestamp().stamp(), moment().format('YY-MM-DD HH:mm'));
+})
 it('should ', function () {
 	assert.strictEqual(timestamp(), moment().format('YY-MM-DD HH:mm'));
 });
