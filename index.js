03b34c8ed58dfc06c8f47192b2d8366d6e30fae9
Richard Littauer <richard.littauer@gmail.com>
2016-01-06 18:40:17 -0500

    Fixed since to include 0

--- index.js
+++ index.js
@@ -20,29 +20,43 @@ module.exports = function () {
 
     var times = []
 
+    function getDayDifference (timestamp) {
+      if (timestamp && moment(timestamp, format).isValid()) {
+        const test = Math.abs(
+          getDifference([
+            moment().format(format),
+            moment(timestamp.split(' ')[0], 'YY-MM-DD')
+          ], 'day')
+        )
+        return test
+      }
+    }
+
     rl.on('line', function (line) {
       let timePeriod = line.split('--')
       if (!opts.now && timePeriod.length === 2 && durationValid(timePeriod)) {
         if (
-          !opts.today && !opts.since ||
+          !opts.today && (opts.since !== 0 && !opts.since) ||
           (opts.today && moment().isSame(moment(timePeriod[1], format), 'day')) ||
-          (opts.since && Math.abs(getDifference([moment().format(format), timePeriod[1]], 'day')) <= opts.since)
+          ((opts.since === 0 || opts.since) && getDayDifference(timePeriod[0]) <= parseInt(opts.since))
         ) {
           let difference = getDifference(timePeriod, 'minutes')
           console.log(difference)
           times.push(difference)
         }
-      } else if (
-        opts.now && moment(timePeriod[0], format).isValid() &&
-        timePeriod.length === 2 && timePeriod[1] === ''
-      ) {
+      }
+      if (moment(timePeriod[0], format).isValid() && timePeriod.length === 2 && timePeriod[1] === '') {
         let difference = getDifference([timePeriod[0], moment().format(format)], 'minutes')
-        console.log('Duration so far:\n%s hours and %s minutes', Math.floor(difference / 60), difference % 60)
+        times.push(difference)
+        if (!opts.now) console.log(difference)
+        if (opts.now) {
+          console.log('Duration so far:\n%s hours and %s minutes', Math.floor(difference / 60), difference % 60)
+        }
       }
     })
 
     rl.on('close', function () {
-      if (opts.sum !== false && times.length !== 0) {
+      if (!opts.now && opts.sum !== false && times.length !== 0) {
         let minutes = times.reduce(function (a, b) {
           return a + b
         })
